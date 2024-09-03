38d503cf29d06182e09ee9e2ec1e6aa822154531
RIFKI MAULANA <146139031+rifkimaulanaa@users.noreply.github.com>
2024-09-03 14:11:55 +0000

    V2

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,7 +56,7 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com/rifkimaulanaa">
+          <Link href="https://github.com/rifkimaulanaa">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="https://www.linkedin.com/in/rifki-maulana-19a471207/">
