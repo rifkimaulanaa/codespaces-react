91bdbf7c83b266dfd64973d3dbaffcd0740b8f1e
RIFKI MAULANA <146139031+rifkimaulanaa@users.noreply.github.com>
2024-09-03 14:02:52 +0000

    V2

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
